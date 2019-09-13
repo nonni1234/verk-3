62dff22f42839bbc15cb0c5d7e7e7a8d0d6d2fae
unknown <nonni1234567@gmail.com>
2019-09-13 09:42:14 +0000

    laga stuff

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask import Flask, render_template
 app = Flask(__name__)
 fretta_listi = [
-    ["Best Birthday party in the world","The coolest fucker in the world is holding a birthday party this friday and all the cool kids are going","newsforschool@kidz.com","birthday.jpg"]
-    ["Minecraft Wins","Minecraft has taken over the world and everything has now become creeper themed","helpUs@notajoke.com","creeper.jpg"]
-    ["Man fucking dies","This dude just fuckin died, drop an F in the chat"."chad@coolkidz.com","rip.jpg"]
-    ["Best game in the world","New research found that the best game in the world is not Fortnite,scientists are shocked","britebomber@epicgames.net","fortnite.png"]
+    ["0","Best Birthday party in the world","The coolest fucker in the world is holding a birthday party this friday and all the cool kids are going","newsforschool@kidz.com","birthday.jpg"],
+    ["1","Minecraft Wins","Minecraft has taken over the world and everything has now become creeper themed","helpUs@notajoke.com","creeper.jpg"],
+    ["2","Man fucking dies","This dude just fuckin died, drop an F in the chat","chad@coolkidz.com","rip.jpg"],
+    ["3","Best game in the world","New research found that the best game in the world is not Fortnite,scientists are shocked","britebomber@epicgames.net","fortnite.png"]
 ]
 @app.route('/')
 def home():
